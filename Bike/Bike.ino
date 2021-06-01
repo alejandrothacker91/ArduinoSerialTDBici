1f4cc078d0c76f6a462263ca4b909b0612f99346
alejandrothackerkolo <44816682+alejandrothackerkolo@users.noreply.github.com>
2021-06-01 17:32:35 -0500

    Create Bike.ino

---
+++ Bike/Bike.ino
@@ -0,0 +1,41 @@
+/*
+  Rotary Encoder - Polling Example
+
+  The circuit:
+  encoder pin A to Arduino pin 2
+  encoder pin B to Arduino pin 3
+  encoder ground pin to ground (GND)
+*/
+
+#include <MD_REncoder.h>
+
+// set up encoder object
+MD_REncoder R = MD_REncoder(2,3);
+
+void setup()
+{
+  Serial.begin(57600);
+  R.begin();
+}
+
+void loop()
+{
+  uint8_t dir = R.read();
+
+  //int fast = R.speed();
+  if (dir) {
+    int fast = R.speed();
+    //delay(1);
+
+
+    Serial.print("direction/");
+    Serial.print(dir, DEC);
+
+    Serial.print("/speed/");
+    Serial.print(fast, DEC);
+
+    Serial.print('\n');
+  }
+
+  delay(1);
+}
